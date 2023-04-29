b6d7494f03149788aaffdc710b23230257ecd176
sin0713 <narisin1124@gmail.com>
2023-04-29 19:13:27 +0900

    [Add]RxJava 挙動確認

--- app/src/main/java/com/example/githubapi/ApiClient.kt
+++ app/src/main/java/com/example/githubapi/ApiClient.kt
@@ -1,9 +1,11 @@
 package com.example.githubapi
 
 import android.util.Log
+import com.example.githubapi.data.pojo.Commit
 import com.example.githubapi.data.pojo.CommitInfo
 import com.squareup.moshi.Moshi
 import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import io.reactivex.rxjava3.core.Single
 import retrofit2.Call
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
@@ -26,17 +28,19 @@ object ApiClient {
         retrofit.create(GitHubService::class.java)
 
 
-    fun getCommits(repository: String)  {
-        val call: Call<List<CommitInfo>> = service.getCommits(OWNER, repository)
-        kotlin.runCatching { call.execute() }
-            .onSuccess { response ->
-                response.body()?.forEach { commit ->
-                    Log.d(TAG, commit.sha)
+    fun getCommits(repository: String): Single<List<CommitInfo>>  {
+        return Single.create { emiter ->
+
+            val call: Call<List<CommitInfo>> = service.getCommits(OWNER, repository)
+            kotlin.runCatching { call.execute() }
+                .onSuccess { response ->
+                    Log.d(TAG, "api request is successful!")
+                    emiter.onSuccess(response.body() ?: listOf())
                 }
-            }
-            .onFailure {
-                Log.d(TAG, "getCommits is Failure")
-            }
+                .onFailure {
+                    Log.d(TAG, "getCommits is Failure")
+                }
+        }
     }
 
 }
--- app/src/main/java/com/example/githubapi/MainActivity.kt
+++ app/src/main/java/com/example/githubapi/MainActivity.kt
@@ -2,6 +2,9 @@ package com.example.githubapi
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
+import io.reactivex.rxjava3.schedulers.Schedulers
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
 
@@ -10,9 +13,17 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-
-        GlobalScope.launch {
-            ApiClient.getCommits("bookers")
-        }
+        ApiClient.getCommits("bookers")
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .filter { list -> list.isNotEmpty()}
+            .subscribe { commitList ->
+                val res: StringBuilder = StringBuilder()
+                commitList.forEach {
+                    res.append("${it.sha}\n")
+                }
+                val resultView = findViewById<TextView>(R.id.resultTxt)
+                resultView.text = res
+            }
     }
 }
